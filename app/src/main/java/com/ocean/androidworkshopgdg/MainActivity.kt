dc2923127ba437e0bc81b60cff6d7dd9cfbb8588
PRAGYANSHREE DAS <pragyan9423@gmail.com>
2022-09-15 23:55:47 +0530

    Creating a performant lazy list

--- app/src/main/java/com/ocean/androidworkshopgdg/MainActivity.kt
+++ app/src/main/java/com/ocean/androidworkshopgdg/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -11,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ocean.androidworkshopgdg.ui.theme.AndroidWorkShopGDGTheme
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
 
@@ -78,8 +80,13 @@ private fun OnBoardingScreen(onContinueClicked: () -> Unit) {
 }
 @Composable
 private fun Greetings(names: List<String> = List(1000){"$it"}) { //todo: names: List<String> = listOf("World", "Compose")
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+//    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+//        for (name in names) {
+//            Greeting(name = name)
+//        }
+//    }
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)){
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
